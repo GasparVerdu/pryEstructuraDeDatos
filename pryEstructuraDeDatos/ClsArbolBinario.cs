149589dc8d55f1db2fe9b52ded0eedd0edc7a6c2
root <mgiraudo@ies21.edu.ar>
2023-04-28 11:45:07 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- pryEstructuraDeDatos/ClsArbolBinario.cs
+++ pryEstructuraDeDatos/ClsArbolBinario.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace pryEstructuraDeDatos
 {
@@ -15,5 +16,99 @@ namespace pryEstructuraDeDatos
             get { return inicio; }
             set { inicio = value; }
         }
+        public void agregar(ClsNodo nuevo)
+        {
+            nuevo.izquierda = null;
+            nuevo.derecha = null;
+
+            if (raiz==null)
+            {
+                raiz = nuevo;
+            }
+            else
+            {
+                ClsNodo NodoPadre = raiz;
+                ClsNodo aux = raiz;
+                while (aux !=null)
+                {
+                    NodoPadre = aux;
+                    if (nuevo.Codigo < aux.Codigo)
+                    {
+                        aux = aux.izquierda;
+                    }
+                    else
+                    {
+                        aux = aux.derecha;
+                    }
+                }
+                if (nuevo.Codigo< NodoPadre.Codigo)
+                {
+                    NodoPadre.izquierda = nuevo;
+                }
+                else
+                {
+                    NodoPadre.derecha = nuevo;
+                }
+            }
+        }
+
+        public void Recorrer(ListBox lista)
+        {
+            lista.Items.Clear();
+            InOrdenASC(lista, raiz);
+            
+        } 
+        public void InOrdenASC(ListBox lst , ClsNodo R)
+        {
+            if (R.izquierda != null)
+            {
+                InOrdenASC(lst, R.izquierda);
+            }
+            lst.Items.Add(R.Codigo);
+            if (R.derecha != null)
+            {
+                InOrdenASC(lst, R.derecha);
+            }
+        }
+        public void InOrdenDSC(ListBox lst, ClsNodo R)
+        {
+            if (R.derecha != null)
+            {
+                InOrdenDSC(lst, R.derecha);
+            }
+            lst.Items.Add(R.Codigo);
+            if (R.izquierda != null)
+            {
+                InOrdenDSC(lst, R.izquierda);
+            }
+        }
+
+        public void InOrdenPRE(ListBox lst, ClsNodo R)
+        {
+            lst.Items.Add(R.Codigo);//R
+            if (R.izquierda != null)
+            {
+                InOrdenPRE(lst, R.izquierda);//I
+            }
+            if (R.derecha != null)
+            {
+                InOrdenPRE(lst, R.derecha);//D
+            }
+        }
+
+        public void InOrdenPost(ListBox lst, ClsNodo R)
+        {
+            
+            if (R.izquierda != null)
+            {
+                InOrdenPost(lst, R.izquierda);//I
+            }
+            if (R.derecha != null)
+            {
+                InOrdenPost(lst, R.derecha);//D
+            }
+            lst.Items.Add(R.Codigo);//R
+        }
+
     }
 }
